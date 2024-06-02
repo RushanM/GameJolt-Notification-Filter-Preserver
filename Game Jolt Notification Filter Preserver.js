45f0f48c8dfc29f113f258761f3fc991ba32c8ca
Deflecta <firvelfrivol@gmail.com>
2024-06-02 23:49:21 +0300

    Выпуск!

---
+++ Game Jolt Notification Filter Preserver.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name           Game Jolt Notification Filter Preserver
+// @name:ru        Сохранение фильтров уведомлений Game Jolt
+// @namespace      http://tampermonkey.net/
+// @version        1.0
+// @icon           https://s.gjcdn.net/img/favicon.png
+// @description    Preserves and restores selected filters on the notifications page of Game Jolt.
+// @description:ru Восстанавливает выбранные фильтры на странице уведомлений Game Jolt.
+// @author         Deflecta, GKProduction
+// @match          https://gamejolt.com/*
+// @match          https://*.gamejolt.com/*
+// @grant          none
+// @license        MIT
+// ==/UserScript==
+
+(function() {
+    // Задаём переменные
+    const notifPageUrl = 'https://gamejolt.com/notifications';
+
+    // Сохраняем фильтры
+    function saveFilters()
+        {
+            const urlParams = new URLSearchParams(window.location.search);
+            const filters = urlParams.get('f');
+            if (filters)
+                {
+                    localStorage.setItem('gjNotifFilters', filters);
+                }
+        }
+
+    // Загружаем фильтры
+    function loadFilters()
+        {
+            return localStorage.getItem('gjNotifFilters');
+        }
+
+    // Проверяем факт нажатия
+    document.addEventListener('click', (event) =>
+        {
+            const viewAllButton = event.target.closest('a.button.-trans.-block');
+            if (viewAllButton && viewAllButton.textContent.trim() === ('View all' || 'Посмотреть всё'))
+                {
+                    const filters = loadFilters();
+                    if (filters)
+                        {
+                            event.preventDefault();
+                            window.location.href = `${notifPageUrl}?f=${filters}`;
+                        }
+                }
+        });
+
+    // Запоминаем фильтры в нужный момент с MutationObserver
+    const observer = new MutationObserver(() =>
+        {
+            if (window.location.href.startsWith(notifPageUrl))
+                {
+                    saveFilters();
+                }
+        });
+
+    // Запускаем MutationObserver
+    observer.observe(document.body,
+        {
+            childList: true, subtree: true 
+        });
+
+    // Сохраняем фильтры, если мы в уведомлениях
+    if (window.location.href.startsWith(notifPageUrl))
+        {
+            saveFilters();
+        }
+
+})();
---
+++ README.md
@@ -0,0 +1,21 @@
+# Game Jolt Notification Filter Preserver
+
+A [Tampermonkey](https://github.com/Tampermonkey/tampermonkey) script that saves your notification filters on [Game Jolt](https://gamejolt.com/). How it works: you go to the notifications page by clicking "View all", choose your filters, and leave the page. The next time you visit the notifications page through the "View all" button, the script will automatically redirect you to the page with your previously set filters. It's THAT simple!
+
+Current version: **1.0.**
+
+## Installation
+
+1. Install Tampermonkey
+2. Open the script link from [GitHub](https://github.com/RushanM/GameJolt-Russian-Translation/raw/main/%D0%A0%D1%83%D1%81%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82%D0%BE%D1%80%20Game%20Jolt.user.js) or [GreasyFork](https://greasyfork.org/ru/scripts/496844-game-jolt-russian-translation)
+3. Refresh the Game Jolt page if it was already open
+4. Done!
+
+## Support My Work
+If you appreciate my work and would like to support me, you can do so through the following links:
+* [Boosty](https://boosty.to/rushanm)
+* [Donation Alerts](https://www.donationalerts.com/r/deflecta)
+* [YooMoney](https://yoomoney.ru/to/410015215253910)
+* [VK Pay](https://vk.me/moneysend/deflecta)
+
+I am grateful for every contribution!
